7b136c8cb1ccf6d4f016ff707b75dfac870f0b89
Jay Harris <jay.harris@outlook.co.nz>
2017-03-30 17:52:51 +1300

    Maybe stuff works?

--- status.py
+++ status.py
@@ -2,15 +2,27 @@ import requests
 
 import router
 
+disconnected_status = 'constWANNotConnected'
+connected_status = 'constWANConnected'
+
 def get_status():
     url = router.status_url()
     request_id = '1' # the router doesn't care but this has to be a thing
-    response = requests.get(url, params={
-            'RequestID':request_id
-        })
+    try:
+        response = requests.get(url, params={
+                'RequestID':request_id
+            })
 
-    json = response.json()
-    return 'connected' if json['Status'] == 'constWANConnected' else 'disconnected'
+        if not response.status_code == 200:
+            return "disconnected"
+
+        json = response.json()
+        if json['Status'] == 'constWANConnected':
+            return 'connected'
+        else:
+            return 'disconnected'
+    except:
+        return 'down'
 
 if __name__ == "__main__":
     print(get_status())
--- watcher.py
+++ watcher.py
@@ -5,22 +5,30 @@ import restart
 import status
 
 class Watcher:
-    def __init__(self, check_rate=30, restart_rate=120):
+    def __init__(self, check_rate=30, max_restart_rate=180):
         """Initializes a new watcher"""
 
         self.check_rate = check_rate
-        self.restart_rate = 120
+        self.max_restart_rate = max_restart_rate
+        self.last_check = None
 
     def run(self):
-        self.last_restart = None
-        
+        """Begins running the watcher (witch will run forever)"""
         while True:
+            print("Checking...")
             self.last_check = datetime.now()
+            till_next_check = self.check_rate
 
-            s = status.get_status()
+            current_status = status.get_status()
 
-            to_sleep = max(self.check_rate - (datetime.now() - self.last_check).seconds, 0)
+            # if we aren't connected, restart the router
+            if current_status == "disconnected":
+                till_next_check = self.max_restart_rate
+                restart.restart()
+                print("Restarted!")
+
+            to_sleep = max(till_next_check - (datetime.now() - self.last_check).seconds, 0)
             time.sleep(to_sleep)
 
-w = Watcher(5)
+w = Watcher(10)
 w.run()
