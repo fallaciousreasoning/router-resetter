c69a941a8ae8f837e324a621b611002ff474898e
Jay Harris <jay.harris@outlook.co.nz>
2017-03-30 17:15:00 +1300

    Adds a module for restarting the router

---
+++ restart.py
@@ -0,0 +1,19 @@
+import requests
+import router
+
+def restart():
+    """Restarts the router"""
+    url = router.restart_url()
+    data = {
+        'todo': 'save',
+        'flush_to_page': 0,
+        'this_file': 'ut_reset.html',
+        'next_file': '000-Dashboard.htm',
+        'message': ''
+    }
+
+    response = requests.post(url, data)
+    print(response.text)
+
+if __name__ == '__main__':
+    restart()
--- router.py
+++ router.py
@@ -1,9 +1,14 @@
 router_url = "http://router/"
+
 reset_endpoint = "ut_reset.html"
+restart_endpoint = "setup.cgi"
 status_endpoint = "BelkinAPI/DBWANStatus"
 
 def reset_url():
     return router_url + reset_endpoint
+
+def restart_url():
+    return router_url + restart_endpoint
 
 def status_url():
     return router_url + status_endpoint
